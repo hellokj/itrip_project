ab6f122c46d3f7e8ff3a4d4de93f2f4a65239bc1
ryanchen34057 <ryanchen34057@gmail.com>
2019-07-05 16:46:18 +0800

    no message

--- models/spot.js
+++ models/spot.js
@@ -31,10 +31,10 @@ spotSchema.indexes({location: '2dsphere'});
 
 // sortBy => checkins, ig_post_num
 spotSchema.statics.getSpots = function(place, category, name, sortBy, page, limit, order) {
-    if(name != undefined) {
+    if(name != "") {
         return this.find({$or:[{name:{$regex:name,$options:"$i"}}, {wiki_name:{$regex:name,$options:"$i"}}]}).sort({checkins: -1}).limit(10);
     }
-    else if(category != undefined){
+    else if(category != ""){
         return this.paginate({$or: Place_query_shortener(place), category: category}, Options(sortBy, page, limit, order), 
         function(err, result) {
             return result;
