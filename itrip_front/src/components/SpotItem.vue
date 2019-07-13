411d03f96152dfa35d03b5d6923a1080aaf297c6
ryanchen34057 <ryanchen34057@gmail.com>
2019-07-13 23:02:32 +0800

    added social icon

--- itrip_front/src/components/SpotItem.vue
+++ itrip_front/src/components/SpotItem.vue
@@ -3,15 +3,18 @@
         <b-container>
             <b-row class="ml-0 my-0 px-0 py-0">
                 <b-col class="ml-0 my-0 pl-0 py-0" cols="3.8">
-                    <img class="ml-0 my-0 px-0 py-0 spot-picture" v-bind:src="spot.images[0]" alt="Picture">
+                    <img class="ml-0 my-1 px-0 py-0 spot-picture" :src="spot.images[0]" alt="Picture">
                 </b-col>
-                <b-col cols="8" class="mx-0 my-0 px-0 py-0">
+                <b-col cols="7" class="mx-0 my-0 px-0 py-0">
                     <p class="mx-0 my-0 px-0 py-0 p-name">{{spotIndex}}. {{spot.name}}</p>
-                    <p class="mx-0 my-0 px-0 py-0">{{ getAddress() }}</p>
-                    <!-- <button><i src=""></i></button>
-                    <button><i src=""></i></button> -->
+                    <p class="mx-0 my-1 px-0 py-0">{{ getAddress() }}</p>
+                    <b-row class="mx-0 my-4 px-0 py-0">
+                        <i class="mx-0 my-0 px-0 py-0 fas fa-blog"></i>
+                        <i class="mx-3 my-0 px-0 py-0 fab fa-facebook-square"></i>
+                        <img class="mx-1 my-0 px-0 py-0 instagram" src="../assets/instagram.png">
+                    </b-row>
                 </b-col>
-                <b-col class="mx-0 my-0 px-0 py-0">
+                <b-col class="ml-4 my-0 px-0 py-0">
                     <i class="fas fa-plus-square" @click="$emit('add-spot', spot)"></i>
                 </b-col>
             </b-row>
@@ -60,6 +63,7 @@ export default {
 <style scoped>
     .container {
         align-self: center;
+        height: 130px;
     }
     .spot-item {
         margin-left: 10px;
@@ -73,25 +77,43 @@ export default {
 
     }
     .spot-picture{
-        width: 110px;
-        height: 110px;
+        width: 120px;
+        height: 120px;
         margin-right: 10px;
     }
     .p-name {
         font-size:20px;
     }
-    .btn-add {
-        z-index: 10;
-        position: absolute;
-        margin-left: 330px;
-        margin-top: 10px;
-        width: 20px;
-        height: 20px;
+
+    p {
+        overflow:hidden;
+        white-space:nowrap;
+        -ms-text-overflow:ellipsis;
+        text-overflow:ellipsis;
+        width:270px;
+        height:1.5em;
+    }
+    p:hover {
+        overflow:visible;
+    }
+    .fa-blog {
+        font-size: 25px;
+        color:darkorange;
         cursor: pointer;
     }
     .fa-plus-square {
         font-size: 25px;
         color:darkgray;
+        cursor: pointer;
+    }
+    .fa-facebook-square {
+        font-size: 30px;
+        color:#3b5998;
+        cursor: pointer;
+    }
+    .instagram {
+        width:30px;
+        height:30px;
         cursor: pointer;
     }
 </style>
