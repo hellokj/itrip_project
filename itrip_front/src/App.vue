828db0e1affe18078f03a67aafba01d1178dcba0
ryanchen34057 <ryanchen34057@gmail.com>
2019-07-22 11:16:42 +0800

    fixed togos and center spot mystery bug...

--- itrip_front/src/App.vue
+++ itrip_front/src/App.vue
@@ -25,7 +25,8 @@ export default {
       param: {},
       mobileMode: false,
       windowWidth: 0,
-      atHome: true
+      atHome: true,
+      isAuthorized: this.$store.state.isAuthorized,
     }
   },
   methods: {
